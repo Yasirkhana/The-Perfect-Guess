8e1d3d237d400f7901b831edea8e84f702b3dfb7
Yasir Ahmad <khan10055yasir@gmail.com>
2021-05-21 07:33:06 +0500

    Final Commit
    DONE!

--- ThePerfectGuess.py
+++ ThePerfectGuess.py
@@ -1,5 +1,31 @@
+#                               THE PERFECT GUESS GAME :
 
 import random
 rand = random.randint(0,100)
+userscore = 1
+# print(rand)
 
-print(rand)
+
+while userscore <= 10:
+    user = int(input("Guess the number : "))
+
+    if(user == rand):
+        print(f'''YOU guess it right BRAWO
+                YOUR SCORE :  {userscore}  ''')
+        break
+
+    elif (user < rand):
+        userscore += 1
+        print(" INSCREASE PLEASE !!")
+        
+
+    elif (user > rand):
+        userscore += 1
+        print(" DECREASE PLEASE !!")
+        
+            
+else:
+    print("YOU LOST !")
+    
+
+
